00bb6897d2170f27e1127407f0e516e01724af2d
Anurag Mittal <anurag.213@gmail.com>
2017-04-20 17:56:35 -0700

    level 1 firstword complete

--- Level1/first_word/first_word.c
+++ Level1/first_word/first_word.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   aff_a.c                                            :+:      :+:    :+:   */
+/*   first_word.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: amittal <amittal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/04/20 17:02:33 by amittal           #+#    #+#             */
-/*   Updated: 2017/04/20 17:16:17 by amittal          ###   ########.fr       */
+/*   Created: 2017/04/20 17:33:21 by amittal           #+#    #+#             */
+/*   Updated: 2017/04/20 17:42:04 by amittal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,46 @@
 
 void	ft_putchar(char c)
 {
-	write (1, &c, 1);
+	write(1, &c, 1);
 }
 
-int		ft_check_a(char *str)
+char	*ft_whitespaces(char *ch)
 {
-	while(*str)
+	while (*ch == ' ' || *ch == '\t')
+		ch++;
+	return(ch);
+}
+
+void	ft_print_first_word(char *str)
+{
+	while (*str  && *str != ' ' && *str != '\t')
 	{
-		if (*str == 'a')
-			return (1);
+	  	ft_putchar(*str);
 		str++;
 	}
+	ft_putchar('\n');
+}
+
+
+int main(int argc, char **argv)
+{
+	char *ptr;
+
+	ptr = argv[1];
+	if (argc != 2)
+	{
+		ft_putchar('\n');
+		return (0);
+	}
+	if (argv[1][0] == ' ' || argv[1][0] == '\t')
+		ptr = ft_whitespaces(argv[1]);
+	ft_print_first_word(ptr);
 	return (0);
 }
 
-int 	main (int argc, char **argv)
-{
-	if (argc != 2)
-	{
-		ft_putchar('a');
-		ft_putchar('\n');
-		return(0);
-	}
-	if (ft_check_a(argv[1]) == 1)
-		ft_putchar('a');
-	ft_putchar('\n');
-	return(0);
-}
+
+
+
+
+
+
