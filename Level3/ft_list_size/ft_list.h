d5022e42e93e583c58c2ca1c73540e2ca7836dd4
anurag4DSB <anurag.213@gmail.com>
2017-04-27 19:25:17 -0700

    started linked lists finally

---
+++ Level3/ft_list_size/ft_list.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amittal <amittal@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/04/27 19:03:07 by amittal           #+#    #+#             */
+/*   Updated: 2017/04/27 19:05:33 by amittal          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LIST_H
+# define FT_LIST_H
+
+typedef struct	s_list
+{
+	struct s_list	*next;
+	void			*data;
+}					t_list
+
+int		ft_list_size(t_list *begin_list)
+
+#endif
